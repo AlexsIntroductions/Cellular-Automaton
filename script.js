4e7e63bb48f92f90a9c7db484dead3a0418d4d96
AlexsIntroductions <99214431+AlexsIntroductions@users.noreply.github.com>
2022-06-10 13:37:07 -0400

    Add files via upload
    Added two new modes - random and fractal
    Original Mode renamed to elementary
    Added Board Sizes - small, medium, large, extra large
    
    TODO:
    Ability to enter 'seeds' for the conditions
    Better Logic with reset, next, and animate buttons.

--- script.js
+++ script.js
@@ -14,10 +14,15 @@ var ctx = mainCanvas.getContext("2d");
 var bw;
 var bh;
 var p;
+let cellSize = 40;
 
 //initialize board tracking parameters;
 let i = 0;
-
+let n = 20 * cellSize;
+let m = 40 * cellSize;
+let setting = 2;
+let rando = new Array(8);
+/*
 let n = prompt("Enter number of Rows", "");
 while(isNaN(parseInt(n))){
 	n = prompt("Enter number of Rows", "");
@@ -31,6 +36,7 @@ while(isNaN(parseInt(n))){
 }
 
 m = Math.floor(m) * 40;
+*/
 
 mainCanvas.height = n;
 mainCanvas.width = m;
@@ -51,6 +57,23 @@ function Parents(x, y, z){
 
 //---------------OBJECT FUNCTIONS---------------//
 Parents.prototype.evaluate = function(){
+	switch(setting){
+		case 1:
+			return this.evaluateFractal();
+			break;
+		case 2:
+			return this.evaluateElementary();
+			break;
+		case 3:
+			return this.evaluateRandom();
+			break;
+		default:
+			console.log("BROKEN IN EVALUATE");
+			break;
+	}
+}
+
+Parents.prototype.evaluateElementary = function(){
 	if(this.x == 0){
 		if(this.y == 0){
 			if(this.z == 0){ /*000*/ return 0; }
@@ -72,6 +95,49 @@ Parents.prototype.evaluate = function(){
 	}
 }
 
+Parents.prototype.evaluateFractal = function(){
+	if(this.x == 0){
+		if(this.y == 0){
+			if(this.z == 0){ /*000*/ return 0; }
+			else{ /*001*/ return 1;}
+		}
+		else{
+			if(this.z == 0){ /*010*/ return 0; }
+			else{ /*011*/ return 1; }
+		}
+	}else{
+		if(this.y == 0){
+			if(this.z == 0){ /*100*/ return 1; }
+			else{ /*101*/ return 0; }
+		}
+		else{
+			if(this.z == 0){ /*110*/ return 1; }
+			else{ /*111*/ return 0; }
+		}
+	}
+}
+
+Parents.prototype.evaluateRandom = function(){
+	if(this.x == 0){
+		if(this.y == 0){
+			if(this.z == 0){ /*000*/ return rando[0]; }
+			else{ /*001*/ return rando[1]; }
+		}
+		else{
+			if(this.z == 0){ /*010*/ return rando[2]; }
+			else{ /*011*/ return rando[3]; }
+		}
+	}else{
+		if(this.y == 0){
+			if(this.z == 0){ /*100*/ return rando[4]; }
+			else{ /*101*/ return rando[5]; }
+		}
+		else{
+			if(this.z == 0){ /*110*/ return rando[6]; }
+			else{ /*111*/ return rando[7]; }
+		}
+	}
+}
 //---------------FUNCTIONS---------------//
 
 function onClick(smallCanvas, event){
@@ -79,18 +145,19 @@ function onClick(smallCanvas, event){
 	let x = Math.floor(event.clientX - rect.left);
 	let y = Math.floor(event.clientY - rect.top);
 	//40 wide, only need top x and left y
-	//to get x: x / 40 floor?
-	x = Math.floor(x / 40);
-	y = Math.floor(y / 40);
+	//to get x: x / cellSize floor?
+	x = Math.floor(x / cellSize);
+	y = Math.floor(y / cellSize);
 	
 	sctx.beginPath();
-	sctx.rect(x * 40 + 0.5, y * 40 + 0.5, 40, 40);
+	sctx.rect(x * cellSize + 0.5, y * cellSize + 0.5, cellSize, cellSize);
 	sctx.fill();
 	
 	console.log(x + ", " + y);
 }
 
 function drawBoard(){
+	initRandom();
 	//creating grid on main canvas
 	// Box width
 	bw = mainCanvas.width;
@@ -98,12 +165,12 @@ function drawBoard(){
 	bh = mainCanvas.height;
 	// Padding
 	p = 0;
-    for (var x = 0; x <= bw; x += 40) {
+    for (var x = 0; x <= bw; x += cellSize) {
         ctx.moveTo(0.5 + x + p, p);
         ctx.lineTo(0.5 + x + p, bh + p);
     }
 
-    for (var x = 0; x <= bh; x += 40) {
+    for (var x = 0; x <= bh; x += cellSize) {
         ctx.moveTo(p, 0.5 + x + p);
         ctx.lineTo(bw + p, 0.5 + x + p);
     }
@@ -117,12 +184,12 @@ function drawBoard(){
 	bh = smallCanvas.height;
 	// Padding
 	p = 0;
-    for (var x = 0; x <= bw; x += 40) {
+    for (var x = 0; x <= bw; x += cellSize) {
         sctx.moveTo(0.5 + x + p, p);
         sctx.lineTo(0.5 + x + p, bh + p);
     }
 
-    for (var x = 0; x <= bh; x += 40) {
+    for (var x = 0; x <= bh; x += cellSize) {
         sctx.moveTo(p, 0.5 + x + p);
         sctx.lineTo(bw + p, 0.5 + x + p);
     }
@@ -131,6 +198,7 @@ function drawBoard(){
 }
 
 function doRow(){
+
 	//initialize needed variables
 	let x = -1;
 	let y = -1;
@@ -141,28 +209,28 @@ function doRow(){
 		start();
 		return;
 	}
-	for(let j = 0; j < mainCanvas.width; j+=40){
+	for(let j = 0; j < mainCanvas.width; j+=cellSize){
 		//if i == 1 or i + 40 == maincanvas.width
 		//three times
 	//above i - 20: j + 20 
-		ImageData = ctx.getImageData(j + 20, i - 20, 1, 1);
+		ImageData = ctx.getImageData(j + (cellSize / 2), i - (cellSize / 2), 1, 1);
 		if(ImageData.data[3] == 255){ y = 1; }
 		else{ y = 0; }
 
 	//left i - 20: j - 20
 		//check to make sure j isnt out of bounds of the canvas
-		if(j - 20 < 0){ x = 0; }
+		if(j - (cellSize / 2) < 0){ x = 0; }
 		else{
-			ImageData = ctx.getImageData(j - 20, i - 20, 1, 1);
+			ImageData = ctx.getImageData(j - (cellSize / 2), i - (cellSize / 2), 1, 1);
 			if(ImageData.data[3] == 255){ x = 1; }
 			else{ x = 0; }
 		}
 
 	//right i - 20: j + 60
 		//check to make sure j isnt out of bounds of the canvas
-		if(j + 60 > mainCanvas.width){ z = 0; }
+		if(j + cellSize + (cellSize / 2) > mainCanvas.width){ z = 0; }
 		else{
-			ImageData = ctx.getImageData(j + 60, i - 20, 1, 1);
+			ImageData = ctx.getImageData(j + cellSize + (cellSize / 2), i - (cellSize / 2), 1, 1);
 			if(ImageData.data[3] == 255){ z = 1; }
 			else{ z = 0; }
 		}
@@ -172,20 +240,25 @@ function doRow(){
 
 		if(result == 1){
 			ctx.beginPath();
-			ctx.rect(j, i, 40, 40);
+			ctx.rect(j, i, cellSize, cellSize);
 			ctx.fill();
 		}
 	}
-	i += 40;
+	i += cellSize;
 }
 
-function animationFrame(){
-	doRow();
+function initRandom(){
+	for(let k = 0; k < 8; k++){
+		rando[k] = Math.floor(Math.random() * 2);
+	}
+	for(let k = 0; k < 8; k++){
+		console.log(rando[k]);
+	}
 }
 
 function start(){
 	ctx.drawImage(smallCanvas, 0, 0);
-	i += 40;
+	i += cellSize;
 }
 
 function reset(){
@@ -206,6 +279,62 @@ function step() {
 	}
 }
 
+function boardSmall(){
+	cellSize = 40;
+	n = 20 * cellSize;
+	m = 40 * cellSize;
+	mainCanvas.height = n;
+	mainCanvas.width = m;
+	smallCanvas.width = m;
+	smallCanvas.height = cellSize;
+	reset();
+}
+
+function boardMedium(){
+	cellSize = 20;
+	n = 40 * cellSize;
+	m = 80 * cellSize;
+	mainCanvas.height = n;
+	mainCanvas.width = m;
+	smallCanvas.width = m;
+	smallCanvas.height = cellSize;
+	reset();
+}
+
+function boardLarge(){
+	cellSize = 10;
+	n = 80 * cellSize;
+	m = 160 * cellSize;
+	mainCanvas.height = n;
+	mainCanvas.width = m;
+	smallCanvas.width = m;
+	smallCanvas.height = cellSize;
+	reset();
+}
+
+function boardExtraLarge(){
+	cellSize = 5;
+	n = 160 * cellSize;
+	m = 320 * cellSize;
+	mainCanvas.height = n;
+	mainCanvas.width = m;
+	smallCanvas.width = m;
+	smallCanvas.height = cellSize;
+	reset();
+}
+
+
+function setFractal(){
+	setting = 1;
+}
+
+function setElementary(){
+	setting = 2;
+}
+
+function setRandom(){
+	setting = 3;
+}
 //---------------MAIN PROGRAM---------------//
 
 drawBoard();
